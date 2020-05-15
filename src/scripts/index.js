a3c61846bfee4421f1a3d0fb063c75c03f019fd7
daniel santafe <daniel.341911515@ucaldas.edu.co>
2020-05-14 22:00:24 -0500

    agrego codigo html

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,65 +2,148 @@
 
 console.log('webpack starterkit');
 
-let todoItems = [];
+let Datos = [
+   
+    {
+        id: 1,
+        nombre: 'Play Station 4',
+        precio: 1300000,
+        store: 'Exito',
+        imagen: '//media.playstation.com/is/image/SCEA/playstation-4-pro-vertical-product-shot-01-us-07sep16?$native_t$'
+    },
+    {
+        id: 2,
+        nombre: 'Piano',
+        precio: 1000000,
+        store: 'Falabella',
+        imagen: 'https://recursivearts.com/virtual-piano/TemplateData100/realistic-3d-piano-keyboard.webp'
+    },
+    {
+        id: 3,
+        nombre: 'Chair',
+        precio: 800000,
+        store: 'Exito',
+        imagen: 'https://http2.mlstatic.com/silla-de-oficina-gamer-gerencial-giratoria-D_NQ_NP_913116-MCO41364828622_042020-O.webp'
+    },
+    
+]
+let $items = document.querySelector('#items');
+let carrito = [];
+let total = 0;
+let $carrito = document.querySelector('#carrito');
+let $total = document.querySelector('#total');
 
-function updateUI(){
-const container = document.querySelector('.list-group');
-container.innerHTML = '';
-todoItems.forEach( task => {
-paintTask(task);
-})
+function renderItems () {
+    for (let info of Datos) {
+        
+    let miNodo = document.createElement('div');
+        miNodo.classList.add('card', 'col-sm-4');
+        
+    let miNodoCardBody = document.createElement('div');
+        miNodoCardBody.classList.add('card-body');
+       
+    let miNodoTitle = document.createElement('h5');
+        miNodoTitle.classList.add('card-title');
+        miNodoTitle.textContent = info['nombre'];
+        
+    let miNodoImagen = document.createElement('img');
+        miNodoImagen.classList.add('img-fluid');
+        miNodoImagen.setAttribute('src', info['imagen']);
+      
+        
+    let miNodoPrecio = document.createElement('p');
+        miNodoPrecio.classList.add('card-text');
+        miNodoPrecio.textContent = info['precio'] + '$';
+        
+   let miNodoStore = document.createElement('h1');
+        miNodoStore.classList.add('card-title');
+        miNodoStore.textContent = info['store'];
+        
+         
+    let miNodoBoton = document.createElement('button');
+        miNodoBoton.classList.add('btn', 'btn-danger');
+        miNodoBoton.textContent = 'Agregar a carrito';
+        miNodoBoton.setAttribute('marcador', info['id']);
+        miNodoBoton.addEventListener('click', añadirCarrito);
+        
+        miNodoCardBody.appendChild(miNodoImagen);
+        miNodoCardBody.appendChild(miNodoTitle);
+        miNodoCardBody.appendChild(miNodoPrecio);
+        miNodoCardBody.appendChild(miNodoBoton);
+        miNodo.appendChild(miNodoCardBody);
+        $items.appendChild(miNodo);
+    }
+}
+function añadirCarrito () {
+   
+    carrito.push(this.getAttribute('marcador'))
+    calcularTotal();
+    renderizarCarrito();
+
 }
 
-
-function paintTask(task){
-const container = document.querySelector('.list-group');
-const checked = task.isSelected ? 'checked' : '';
-container.insertAdjacentHTML ('beforeend', `
-<li class="list-item list-group-item ${checked}"> 
-<input type="checkbox" ${checked} onchange="changeTask(event,${0})"/>
-<span>${task.name}</span>
-<div class="left">
-<button class="btn btn-sm" onclick="deleteTask(${0})">${eliminar}</button>
-</div>
-</li>
- `);
+function renderizarCarrito () {
+    
+    $carrito.textContent = '';
+ 
+    let carritoSinDuplicados = [...new Set(carrito)];
+    carritoSinDuplicados.forEach(function (item, indice) {
+    
+    let miItem = Datos.filter(function(itemBaseDatos) {
+    return itemBaseDatos['id'] == item;
+    });
+       
+    let numeroUnidadesItem = carrito.reduce(function (total, itemId) {
+    return itemId === item ? total += 1 : total;
+    }, 0);
+      
+    let miNodo = document.createElement('li');
+    miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
+    miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0]['nombre']} - ${miItem[0]['precio']}€`;
+        
+        
+     let miBoton = document.createElement('button');
+     miBoton.classList.add('btn', 'btn-danger', 'mx-5');
+     miBoton.textContent = 'Eliminar de carrito';
+     miBoton.style.marginLeft = '1rem';
+     miBoton.setAttribute('item', item);
+     miBoton.addEventListener('click', borrarItemCarrito);
+        // Mezclamos nodos
+        miNodo.appendChild(miBoton);
+        $carrito.appendChild(miNodo);
+    })
 }
 
-function changeTask(event){
-todoItems[0].isSelected = event.target.checked; 
-updateUI();
-}
-function deleteTask(index){
-todoItems.splice(index, 1);
-updateUI();
-}
-function addTask(){
-const input = document.querySelector('#input-task');
-if(input.value == ''){
-return;
-}
-  
-const task = {
-name : input.value,
-isSelected: false
-};
-todoItems.push(task);
-input.value = '';
-updateUI();
+function borrarItemCarrito () {
+    console.log()
+   
+    let id = this.getAttribute('item');
+    carrito = carrito.filter(function (carritoId) {
+        return carritoId !== id;
+    });
+    
+    renderizarCarrito();
+    calcularTotal();
 }
 
-const eliminar = `<svg class="bi bi-trash" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-  <path d="M5.5 5.5A.5.5 0 016 6v6a.5.5 0 01-1 0V6a.5.5 0 01.5-.5zm2.5 0a.5.5 0 01.5.5v6a.5.5 0 01-1 0V6a.5.5 0 01.5-.5zm3 .5a.5.5 0 00-1 0v6a.5.5 0 001 0V6z"/>
-  <path fill-rule="evenodd" d="M14.5 3a1 1 0 01-1 1H13v9a2 2 0 01-2 2H5a2 2 0 01-2-2V4h-.5a1 1 0 01-1-1V2a1 1 0 011-1H6a1 1 0 011-1h2a1 1 0 011 1h3.5a1 1 0 011 1v1zM4.118 4L4 4.059V13a1 1 0 001 1h6a1 1 0 001-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" clip-rule="evenodd"/>
-</svg>
-`;
+function calcularTotal () {
+    total = 0;
+    for (let item of carrito) {
+        let miItem = Datos.filter(function(itemBaseDatos) {
+            return itemBaseDatos['id'] == item;
+        });
+        total = total + miItem[0]['precio'];
+    }
+    $total.textContent = total.toFixed(2);
+}
+
+renderItems();
 
 $(document).ready(function() {
     $('div').hover(
         function () {
             
-            $(this).css({"background-color":"red"});
+            $(this).css({"background-color":"white"});
         }, 
         function () {
             
@@ -68,3 +151,4 @@ $(document).ready(function() {
         }
     );
 });
+
